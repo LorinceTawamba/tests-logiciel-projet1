0888f6868d38d6fe54042b33b3c05c995c363cc6
Lorince Tawamba <lorince.tawamba@gmail.com>
2023-04-23 07:13:54 -0400

    Update method addition insert generation value

--- src/test/java/org/example/CalculatriceTest.java
+++ src/test/java/org/example/CalculatriceTest.java
@@ -1,18 +1,29 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatriceTest {
+    private double x;
+    private double y;
+    private double r;
+
+    @BeforeEach
+    void setUp() {
+        x=100*Math.random();
+        y=50*Math.random();
+    }
 
     @Test
     void addition() {
         System.out.println("===== TEST ADDITION =====");
+        r = x + y;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("r = " + r);
         // addition(2,4) == 6
-        double x = 2;
-        double y = 4;
-        double r = 6;
         Calculatrice cal = new Calculatrice(x,y);
         // 2, 4, 6
         assertEquals(r, cal.addition());
